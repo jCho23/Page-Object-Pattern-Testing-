c94fb53c2eddcaa908f960ca6bd7f8bf0eeecdeb
JuneCho23 <junecho.ucb@gmail.com>
2016-10-16 11:16:54 -0700

    Code Clean

--- TaskyiOS/AppDelegate.cs
+++ TaskyiOS/AppDelegate.cs
@@ -20,7 +20,6 @@ namespace Tasky
 	[Register("AppDelegate")]
 	public partial class AppDelegate : UIApplicationDelegate
 	{
-		// class-level declarations
 		UIWindow window;
 		UINavigationController navController;
 		UITableViewController homeViewController;
@@ -34,6 +33,7 @@ namespace Tasky
 
 			window.MakeKeyAndVisible();
 
+			//HockeyApp Setup for iOS
 			var hockeyappManager = BITHockeyManager.SharedHockeyManager;
 			hockeyappManager.Configure(HockeyAppConstants.iOSHockeyAppID);
 			hockeyappManager.LogLevel = BITLogLevel.Debug;
@@ -41,32 +41,19 @@ namespace Tasky
 			hockeyappManager.Authenticator.AuthenticateInstallation();
 			hockeyappManager.UpdateManager.CheckForUpdateOnLaunch = true;
 
-
-			// create our nav controller
 			navController = new UINavigationController();
 
-			// create our Todo list screen
 			homeViewController = new Screens.HomeScreen();
 
-
-			//			UIApplication.SharedApplication.KeyWindow.TintColor = UIColor.White;
-			//			navController.NavigationBar.BarTintColor = UIColor.FromRGB (0x91, 0xCA, 0x47);
-			// green theme
-
-			//			navController.NavigationBar.TintColor = UIColor.White;
-			//			navController.NavigationBar.BarTintColor = UIColor.FromRGB (0x6F, 0xA2, 0x2E);
-
-			navController.NavigationBar.TintColor = UIColor.FromRGB(0x6F, 0xA2, 0x2E); // 6FA22E dark-green
-			navController.NavigationBar.BarTintColor = UIColor.FromRGB(0xCF, 0xEF, 0xA7); // CFEFA7 light-green
+			navController.NavigationBar.TintColor = UIColor.FromRGB(0x6F, 0xA2, 0x2E); 
+			navController.NavigationBar.BarTintColor = UIColor.FromRGB(0xCF, 0xEF, 0xA7);
 
 			UINavigationBar.Appearance.SetTitleTextAttributes(new UITextAttributes()
 			{
-				//				TextColor = UIColor.White,
-				TextColor = UIColor.FromRGB(0x6F, 0xA2, 0x2E), // 6FA22E dark-green
+				TextColor = UIColor.FromRGB(0x6F, 0xA2, 0x2E), 
 				TextShadowColor = UIColor.Clear
 			});
 
-			// push the view controller onto the nav controller and show the window
 			navController.PushViewController(homeViewController, false);
 			window.RootViewController = navController;
 			window.MakeKeyAndVisible();
